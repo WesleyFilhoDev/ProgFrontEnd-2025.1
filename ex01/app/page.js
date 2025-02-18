63d51f21a4f515658c8398ae4c09ec02b20db65e
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-02-17 21:03:26 -0300

    criando botão

--- ex01/app/page.js
+++ ex01/app/page.js
@@ -2,6 +2,12 @@ function Texto1() {
   return <p>Um outro parágrafo de texto</p>;
 }
 
+function MyButton() {
+  return (
+    <button>Meu primeiro botão</button>
+  );
+}
+
 export default function Home() {
   const aula = 1;
   return (
@@ -11,6 +17,8 @@ export default function Home() {
       <p>Essa é nossa {aula}ª aula de React</p>
 
       <Texto1></Texto1>
+
+      <MyButton></MyButton>
     </>
-  )
+  );
 }
