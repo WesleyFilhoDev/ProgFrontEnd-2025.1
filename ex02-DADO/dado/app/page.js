d1c44124c5e4ee586025f7d94cfc9c7d61de5bee
WesleyFilhoDev <wesleyfilho221204@gmail.com>
2025-03-12 15:12:06 -0300

    exercicio do componente dado feito

---
+++ ex02-DADO/dado/app/page.js
@@ -0,0 +1,41 @@
+'use client';
+import { useState } from 'react';
+import Image from "next/image";
+
+function Tittle() {
+  return (
+    <h1>Exercício do componente: Dado</h1>
+  )
+}
+
+function MyButton({onClick}) {
+  return (
+    <button onClick = {onClick} style={{ border: "2px solid red", borderRadius: "5px", padding: "10px", cursor: "pointer" }}>GIRAR</button>
+  )
+}
+
+
+function gerarNumAleat() {
+  return Math.floor(Math.random() * 6) + 1;
+}
+
+function GirarDado({src, alt}) {
+  const numAleat = gerarNumAleat();
+  return (
+    <img src={src} alt={alt} style={{ borderRadius: '50px', padding: '20px' }}/>
+  )
+}
+
+export default function Home() {
+  const [random, setRandom] = useState(1);
+  const handleClick = () => {
+    setRandom(gerarNumAleat());
+  }
+  return (
+    <div className="container">
+      <Tittle/>
+      <MyButton onClick={handleClick}/>
+      <GirarDado src={`/dado${random}.jpg`} alt={`Dado ${random}`} />
+    </div>
+  )
+}
